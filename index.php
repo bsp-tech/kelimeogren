b0f7df7dfb0d114dd2c30fc37bb3e4378e5129c2
goshgarmirzayev <qosqarmirze@gmail.com>
2019-11-12 11:01:15 +0400

    Preparing for Server

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home Page</title>
+    <style>
+    
+    </style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    
+    <section class = "bg-warning text-white w-100">
+        <div class="container">
+            <div class="row pt-5">
+                <div class="col-12 text-center mt-5">
+                    <img src="img/2level.png" alt="" width = "60" class = "mt-5">
+                </div>
+                <div class="col-12 text-center">
+                    <p class="h2 text-white font-weight-normal pt-2">İsmini gir</p>
+                </div>
+                <div class="col-12 text-center">
+                    <p class="font-italic text-white m-0">100 puan kazandin.</p>
+                </div>
+                <div class="col-12 d-flex justify-content-center px-5 mb-5">                
+                    <div class="progress w-50"> <div class="progress-bar bg-primary" role="progressbar" style="width: 50%" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100"></div> </div>
+                </div>
+                <div class="col-6 text-center border-right">
+                    <i class="fas fa-graduation-cap fa-3x"></i>
+                    <p class="h4 pb-3">Öğren</p>
+                    <p>%0 tamamlandi.</p>
+                </div>
+                <div class="col-6 text-center">
+                    <i class="far fa-question-circle fa-3x"></i>
+                    <p class="h4 pb-3">Test Et</p>
+                    <p>Henuz test etmedin.</p>
+                </div>
+                <div class="col-6 text-center border-right">
+                    <i class="fas fa-reply-all fa-3x"></i>
+                    <p class="h4 pb-3">Tekrar Et</p>
+                    <p>Henuz kelime ogrenmedin.</p>
+                </div>
+                <div class="col-6 text-center">
+                    <i class="fas fa-trophy fa-3x"></i>
+                    <p class="h4 pb-3">En Iyiler</p>
+                    <p>Bu hafta ilk 50'ye giremedin.</p>
+                </div>
+                <div class="col-12 text-center mt-5 pt-2 pb-5">
+                    <button class="btn btn-outline-light btn-lg">Reklamlari kaldir</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+     <!-- Bootstrap -->
+     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <!--iconlar ucun     Font Awasome-->
+     <script src="https://kit.fontawesome.com/fd540daa68.js"></script>
+     
+</body>
+</html>
